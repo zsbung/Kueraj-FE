efc94c996738954564f751ccc0b8bd10220c695b
agillstrz <agillstrz@gmail.com>
2023-08-09 11:26:03 +0700

    update

--- src/pages/user/About.jsx
+++ src/pages/user/About.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="flex">
+        <div className="w-1/2"></div>
+        <div className="w-1/2 ">
+          <h1 className="text-2xl font-bold text-left capitalize">
+            <span className="">Selamat Datang di</span> BungaBusana
+          </h1>
+          <p className="text-md font-medium">
+            Destinasi mode pilihan Anda untuk pakaian pria dan wanita terbaru
+            dan terkini. Di Bunga Busana, kami berkomitmen untuk memberikan Anda
+            pengalaman berbelanja online yang menarik dan memuaskan, dengan
+            koleksi pakaian yang stylish, nyaman, dan berkualitas tinggi.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
